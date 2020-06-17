91f987c7fed96d5507e9a7c69055b9136b8f0651
8uurg <arthur.guijt12@gmail.com>
2020-06-17 15:06:50 +0200

    Fix shell related issue
    On windows it always returned the same value due to this...

--- expensiveoptimbenchmark/problems/DockerCFDBenchmark.py
+++ expensiveoptimbenchmark/problems/DockerCFDBenchmark.py
@@ -2,18 +2,28 @@ import subprocess
 
 class DockerCFDBenchmarkProblem:
 
-    def __init__(self, name, d, lbs, ubs, vartype, direction):
+    def __init__(self, name, d, lbs, ubs, vartype, direction, errval):
         self.name = name
         self.d = d
         self.lb = lbs
         self.ub = ubs
         self.vt = vartype
         self.direction = 1 if direction == "min" else -1
+        self.errval = errval
     
     def evaluate(self, xs):
         evalCommand = f"docker run --rm frehbach/cfd-test-problem-suite ./dockerCall.sh {self.name}"
-        parsedCandidate = ",".join([str(x) for x in xs])
-        return self.direction * float(subprocess.check_output(f"{evalCommand} '{parsedCandidate}'", shell=True).strip().split(b"\n")[-1])
+        parsedCandidate = ",".join(["%.8f" % x for x in xs])
+        cmd = f"{evalCommand} '{parsedCandidate}'"
+        # print(f"Running '{cmd}'")
+        # res = subprocess.check_output(cmd, shell=True)
+        res = subprocess.check_output(["docker", "run", "--rm", "frehbach/cfd-test-problem-suite", "./dockerCall.sh", self.name, parsedCandidate])
+        reslast = res.strip().split(b"\n")[-1]
+        # print(f"Result: {res}")
+        try:
+            return self.direction * float(reslast)
+        except:
+            return self.errval
 
     def lbs(self):
         return self.lb
@@ -31,18 +41,18 @@ class DockerCFDBenchmarkProblem:
         return f"DockerCFDBenchmark(name={self.name})"
 
 # TODO: Verify that the ESP problem is indeed a minimization problem.
-ESP = DockerCFDBenchmarkProblem("ESP", 49, [0] * 49, [7] * 49, ['int'] * 49, "min")
+ESP = DockerCFDBenchmarkProblem("ESP", 49, [0] * 49, [7] * 49, ['int'] * 49, "min", 1.0)
 
 PitzDaily = DockerCFDBenchmarkProblem("PitzDaily",
     10,
     [-0.01, -0.05, -0.01, -0.05, -0.01, -0.05, -0.01, -0.05, -0.01, -0.05],
     [0.287397, 0.014, 0.287397, 0.014, 0.287397, 0.014 , 0.287397, 0.014, 0.287397, 0.014],
-    ['cont'] * 10, "min")
+    ['cont'] * 10, "min", 1.0)
 
 KaplanDuct = DockerCFDBenchmarkProblem("KaplanDuct", 4, 
     [720.0, -1050.5, 726.99988, -710.0],
     [3609.0, -390.0, 3609.0,  450.0], 
-    ['cont'] * 4, "max")
+    ['cont'] * 4, "max", 1.0)
 
 dockersimbenches = [ESP, PitzDaily, KaplanDuct]
 # Note: HeatExchanger is a 28 dimensional problem, all continuous, unlike previous problems.
