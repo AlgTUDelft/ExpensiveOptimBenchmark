f4427bbb7cb79a0b519b22b4f0d2479068bdf1fe
8uurg <arthur.guijt12@gmail.com>
2020-07-08 12:27:12 +0200

    Verbose patcher

--- expensiveoptimbenchmark/problems/ESP2/patch.sh
+++ expensiveoptimbenchmark/problems/ESP2/patch.sh
@@ -1,5 +1,5 @@
 loc=$(dirname "$0")
 cp $loc/dockerCall.py .
 cp $loc/dockerCall.sh .
-cp $loc/createBafflesDict2.py ./Exeter_CFD_Problems/ESP/
-cp $loc/evaluateSimulation2.py ./Exeter_CFD_Problems/ESP/
+cp $loc/createBafflesDict2.py ./Exeter_CFD_Problems/ESP/createBafflesDict2.py
+cp $loc/evaluateSimulation2.py ./Exeter_CFD_Problems/ESP/evaluateSimulation2.py
