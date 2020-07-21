67678e88233359ebabfe6d81822f970d77a4dfc8
8uurg <arthur.guijt12@gmail.com>
2020-07-21 12:04:05 +0200

    Fix print_exception for CoCaBO

--- expensiveoptimbenchmark/solvers/CoCaBO/wCoCaBo.py
+++ expensiveoptimbenchmark/solvers/CoCaBO/wCoCaBo.py
@@ -108,7 +108,7 @@ def optimize_CoCaBO(problem, max_evals, init_points=24, kernel_mix=0.5, log=None
     except Exception as e:
         mon.end()
         print("An error has occurred while using CoCaBO, terminating early...")
-        traceback.print_exception(e)
+        traceback.print_exception()
         return None, None, mon
         
     return solX, solY, mon
