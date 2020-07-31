8f650f29d1b15e682c95cc3dd5cc30468c62770e
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2020-07-31 11:40:29 +0200

    Changed opacity of error

--- expensiveoptimbenchmark/plot_utils.py
+++ expensiveoptimbenchmark/plot_utils.py
@@ -48,24 +48,26 @@ def plot_iter_file(folder_path, y_feature = 'iter_best_fitness', save_file=None)
         lowerError = pd.to_numeric(mean_fitness_value - sd_fitness_value)
         
         ax.plot(mean_fitness_value.index, mean_fitness_value, label = solver, linewidth=2)
-        ax.fill_between(pd.to_numeric(mean_fitness_value.index), upperError, lowerError, alpha=0.25)
+        ax.fill_between(pd.to_numeric(mean_fitness_value.index), upperError, lowerError, alpha=0.4)
 
 
         print(f"{solver}:\nmean={mean_fitness_value.values[-1]} sd={sd_fitness_value.values[-1]}")
+    
     # Styling
     plt.title(f"Problem {problem}")
     plt.ylabel(convert_feature_label(y_feature))
     plt.xlabel("Iteration index")
-    plt.style.use('seaborn-colorblind')
-    plt.grid(True)
-    plt.xlim(np.min(fitness_value_df.index), np.max(fitness_value_df.index))
+    plt.xlim(mean_fitness_value.index[0], mean_fitness_value.index[-1])
+
 
     # Give all lines different markers
     valid_markers = ([item[0] for item in markers.MarkerStyle.markers.items() if item[1] is not 'nothing' and not item[1].startswith('tick') and not item[1].startswith('caret')])
     for i, line in enumerate(ax.get_lines()):
         line.set_marker(valid_markers[i])
         line.set_markevery(5)
- 
+
+    plt.style.use('seaborn-colorblind')
+    plt.grid(True)
 
     # Display
     plt.legend()
