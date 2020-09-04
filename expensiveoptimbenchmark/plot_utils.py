6f3c5d9cf52779e64f4ad175956ca7d21cd2de26
8uurg <arthur.guijt12@gmail.com>
2020-09-04 14:37:44 +0200

    Modify plotting utilities to plot different problems seperately

--- expensiveoptimbenchmark/plot_utils.py
+++ expensiveoptimbenchmark/plot_utils.py
@@ -19,70 +19,77 @@ def plot_iter_file(folder_path, y_feature = 'iter_best_fitness', save_file=None)
     log_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
     file_list = [f for f in log_files if f.endswith('iters.csv')]
 
-    # Initialise figure
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    
-
     # Read log files
     iter_dfs = pd.concat(pd.read_csv(f) for f in file_list)
 
-    for (_, iter_df) in iter_dfs.groupby(['approach', 'problem']):
-        solver = iter_df['approach'].values[0]
-        problem = iter_df['problem'].values[0]
-        list_exp_id = np.unique(iter_df['exp_id'].values)
+    for (problem, problem_df) in iter_dfs.groupby('problem'):
+            
+        # Initialise figure
+        fig = plt.figure()
+        ax = fig.add_subplot()
 
-        fitness_value_df = pd.DataFrame()
-        for exp_id in list_exp_id:
-            exp_df = iter_df[iter_df['exp_id'] == exp_id]
-            exp_df = exp_df.reset_index()
-            fitness_value_df = pd.concat([fitness_value_df, exp_df[y_feature]], axis=1)
-    
-        try:
-            fitness_value_df = fitness_value_df.apply(lambda x: x.str.strip("[]"))
-            fitness_value_df = fitness_value_df.astype(float)
-        except:
-            pass 
-        mean_fitness_value = fitness_value_df.mean(axis=1)
-        sd_fitness_value = fitness_value_df.std(axis=1)
-        upperError = pd.to_numeric(mean_fitness_value + sd_fitness_value)
-        lowerError = pd.to_numeric(mean_fitness_value - sd_fitness_value)
+        for (solver, iter_df) in problem_df.groupby('approach'):
+            # solver = iter_df['approach'].values[0]
+            # problem = iter_df['problem'].values[0]
+            list_exp_id = np.unique(iter_df['exp_id'].values)
+
+            fitness_value_df = pd.DataFrame()
+            for exp_id in list_exp_id:
+                exp_df = iter_df[iter_df['exp_id'] == exp_id]
+                exp_df = exp_df.reset_index()
+                fitness_value_df = pd.concat([fitness_value_df, exp_df[y_feature]], axis=1)
         
-        ax.plot(mean_fitness_value.index, mean_fitness_value, label = solver, linewidth=2)
-        ax.fill_between(pd.to_numeric(mean_fitness_value.index), upperError, lowerError, alpha=0.4)
+            try:
+                fitness_value_df = fitness_value_df.apply(lambda x: x.str.strip("[]"))
+                fitness_value_df = fitness_value_df.astype(float)
+            except:
+                pass 
+            mean_fitness_value = fitness_value_df.mean(axis=1)
+            sd_fitness_value = fitness_value_df.std(axis=1)
+            upperError = pd.to_numeric(mean_fitness_value + sd_fitness_value)
+            lowerError = pd.to_numeric(mean_fitness_value - sd_fitness_value)
+            
+            ax.plot(mean_fitness_value.index, mean_fitness_value, label = solver, linewidth=2)
+            ax.fill_between(pd.to_numeric(mean_fitness_value.index), upperError, lowerError, alpha=0.4)
 
 
-        print(f"{solver}:\nmean={mean_fitness_value.values[-1]} sd={sd_fitness_value.values[-1]}")
+            print(f"{solver}:\nmean={mean_fitness_value.values[-1]} sd={sd_fitness_value.values[-1]}")
     
-    # Styling
-    plt.title(f"Problem {problem}")
-    plt.ylabel(convert_feature_label(y_feature))
-    plt.xlabel("Iteration index")
-    plt.xlim(mean_fitness_value.index[0], mean_fitness_value.index[-1])
+        # Styling
+        plt.title(f"Problem {problem}")
+        plt.ylabel(convert_feature_label(y_feature))
+        plt.xlabel("Iteration index")
+        plt.xlim(mean_fitness_value.index[0], mean_fitness_value.index[-1])
 
 
-    # Give all lines different markers
-    valid_markers = ([item[0] for item in markers.MarkerStyle.markers.items() if item[1] is not 'nothing' and not item[1].startswith('tick') and not item[1].startswith('caret')])
-    for i, line in enumerate(ax.get_lines()):
-        line.set_marker(valid_markers[i])
-        line.set_markevery(5)
+        # Give all lines different markers
+        valid_markers = ([item[0] for item in markers.MarkerStyle.markers.items() if item[1] is not 'nothing' and not item[1].startswith('tick') and not item[1].startswith('caret')])
+        for i, line in enumerate(ax.get_lines()):
+            line.set_marker(valid_markers[i])
+            line.set_markevery(5)
 
-    plt.style.use('seaborn-colorblind')
-    plt.grid(True)
+        plt.style.use('seaborn-colorblind')
+        plt.grid(True)
 
-    # Display
-    plt.legend()
+        # Display
+        plt.legend()
 
-    if save_file is not None:
-        fig.savefig(save_file)
-    else:
-        plt.show()
+        if save_file is not None:
+            fig.savefig(f"{save_file}_{problem}.png")
+    
+    # Show all at once.
+    # if save_file is None:
+    plt.show()
 
 if __name__ == "__main__":
     import sys
     folder_path = sys.argv[1]
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 2 and sys.argv[2] != '_':
         feature = sys.argv[2]
     else:
         feature = 'iter_best_fitness'
-    plot_iter_file(folder_path, feature)
+    if len(sys.argv) > 3:
+        save_file = sys.argv[3]
+    else:
+        save_file = None
+    plot_iter_file(folder_path, feature, save_file)
